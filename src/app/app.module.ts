7c6e4779a2420c03d7f3ca1bd06b3ef4cbf5ae01
Vinay M <Vinay.M@lntinfotech.com>
2020-06-19 16:34:49 +0530

    counter-ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {StoreModule} from '@ngrx/store';
+import {counterReducer} from './reducers/counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { StoreDevtoolsModule} from '@ngrx/store-devtools';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
